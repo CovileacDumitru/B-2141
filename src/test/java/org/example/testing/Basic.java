773e85117d201acee92c4a692d25f0d9188cea00
PC <gaibuiana126@gmail.com>
2024-11-04 13:10:44 +0200

    second commt

---
+++ src/test/java/org/example/testing/Basic.java
@@ -0,0 +1,11 @@
+package org.example.testing;
+
+public class Basic {
+
+    public static String FIRST_NAME = "Iana";
+    public static String LAST_NAME = "Gaibu";
+    public static String EMAIL = "BSS@gmail.com";
+    public static String MOBILE = "079103072";
+    public static String STATE = "Rajasthan";
+    public static String CITY = "Jaipur";
+}
