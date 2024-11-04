2a3f27301c6e61bc259b6b673b60ddc3cf0b5be3
PC <gaibuiana126@gmail.com>
2024-11-04 13:56:01 +0200

    first commit

--- src/test/java/org/example/testing/FormaTest.java
+++ src/test/java/org/example/testing/FormaTest.java
@@ -12,25 +12,27 @@ public class FormaTest extends Basic {
     public static WebDriver driver;
 
     @BeforeClass
-    public void beforeClass(){
+    public void beforeClass() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Desktop\\chromedriver-win64\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        FormaTest.driver = driver;
+
+
     }
 
     @Test
-    public void FormaTngTest(){
+    public void FormaTest(){
         driver.get("https://demoqa.com/automation-practice-form");
         FormaPage formaPage = new FormaPage(driver);
-        formaPage.setFirstName(FIRST_NAME);
+        formaPage.setFirstName("Anatolie");
     }
 
     @AfterClass
-    public void  close(){
+    public void close() {
         driver.quit();
     }
+
 }
 
